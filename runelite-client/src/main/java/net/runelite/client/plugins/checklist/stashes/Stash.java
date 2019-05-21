e9de1f5e5f632332e426bc5c85ce5bb095a866d3
Collin Bauer <collinlbauer@gmail.com>
2019-05-21 09:06:14 -0400

    STASHes stubbed

--- runelite-client/src/main/java/net/runelite/client/plugins/checklist/stashes/Stash.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/checklist/stashes/Stash.java
@@ -65,34 +65,861 @@ public enum Stash {
         "hole",
         "Outside the shack that leads to Zanaris in Lumbridge Swamp",
         new int[] {ItemID.BRONZE_DAGGER, ItemID.IRON_FULL_HELM,ItemID.GOLD_RING}
-    );
+    ),
+
+    Eas02 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas03 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas04 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas05 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas06 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas07 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas08 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas09 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas10 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas11 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas12 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas13 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas14 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas15 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas16 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas17 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas18 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas19 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas20 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas21 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas22 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas23 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas24 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas25 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas26 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas27 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas28 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eas29 (
+        0,
+        1,
+        "",
+        "",
+        new int[] {}
+    ),
+
 
     /* Medium stashes */
+    Med01 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med02 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med03 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med04 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med05 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med06 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med07 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med08 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med09 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med10 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med11 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med12 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med13 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med14 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med15 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med16 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med17 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med18 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med19 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med20 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med21 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med22 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Med23 (
+        0,
+        2,
+        "",
+        "",
+        new int[] {}
+    ),
+
 
     /* Hard stashes */
+    Har01 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har02 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har03 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har04 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har05 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har06 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har07 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har08 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har09 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har10 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har11 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har12 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har13 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har14 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Har15 (
+        0,
+        3,
+        "",
+        "",
+        new int[] {}
+    ),
+
 
     /* Elite stashes */
+    Eli01 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli02 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli03 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli04 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli05 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli06 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli07 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli08 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli09 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli10 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli11 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli12 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli13 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli14 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli15 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Eli16 (
+        0,
+        4,
+        "",
+        "",
+        new int[] {}
+    ),
+
 
     /* Master stashes */
+    Mas01 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
 
+    Mas02 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas03 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas04 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas05 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas06 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas07 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas08 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas09 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas10 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas11 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas12 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas13 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas14 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas15 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas16 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas17 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas18 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas19 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas20 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    ),
+
+    Mas21 (
+        0,
+        5,
+        "",
+        "",
+        new int[] {}
+    );
+    
 
     /* STASH arrays */
     public static final Stash[] STASHES_BEGINNER  = {Beg01, Beg02, Beg03};
-    public static final Stash[] STASHES_EASY      = {
 
-    };
-    public static final Stash[] STASHES_MEDIUM    = {
+    public static final Stash[] STASHES_EASY      = {Eas01, Eas02, Eas03, Eas04, Eas05, Eas06, Eas07, Eas08, Eas09,
+        Eas10, Eas11, Eas12, Eas13, Eas14, Eas15, Eas16, Eas17, Eas18, Eas19, Eas20, Eas21, Eas22, Eas23, Eas24,
+        Eas25, Eas26, Eas27, Eas28, Eas29};
 
-    };
-    public static final Stash[] STASHES_HARD      = {
+    public static final Stash[] STASHES_MEDIUM    = {Med01, Med02, Med03, Med04, Med05, Med06, Med07, Med08, Med09,
+        Med10, Med11, Med12, Med13, Med14, Med15, Med16, Med17, Med18, Med19, Med20, Med21, Med22, Med23};
 
-    };
-    public static final Stash[] STASHES_ELITE     = {
+    public static final Stash[] STASHES_HARD      = {Har01, Har02, Har03, Har04, Har05, Har06, Har07, Har08, Har09,
+        Har10, Har11, Har12, Har13, Har14, Har15};
 
-    };
-    public static final Stash[] STASHES_MASTER    = {};
+    public static final Stash[] STASHES_ELITE     = {Eli01, Eli02, Eli03, Eli04, Eli05, Eli06, Eli07, Eli08, Eli09,
+        Eli10, Eli11, Eli12, Eli13, Eli14, Eli15, Eli16};
 
-    
+    public static final Stash[] STASHES_MASTER    = {Mas01, Mas02, Mas03, Mas04, Mas05, Mas06, Mas07, Mas08, Mas09,
+        Mas10, Mas11, Mas12, Mas13, Mas14, Mas15, Mas16, Mas17, Mas18, Mas19, Mas20, Mas21};
+
+
     /* STASH variables */
     private final int stashID;          // game object ID; each STASH ID is unique
     private final int tier;             // beginner = 0, easy = 1, and so on
